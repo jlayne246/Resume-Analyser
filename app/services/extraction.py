1bc85388e9e42b29e1595d75bd1baac0e7915817
kaiPC <118703375+KaiEJH@users.noreply.github.com>
2025-10-23 22:01:16 -0400

    removed unecessary file stream call

--- app/services/extraction.py
+++ app/services/extraction.py
@@ -3,14 +3,14 @@ from pypdf import PdfReader
 class PDF:
     def __init__(self, path):
         self.path = path
+
     def extract_text(self):
-        with open(self.path, 'rb') as file:
-            reader = PdfReader(file)
-            text = ''
-            for page in reader.pages:
-                text += page.extract_text() + '\n'
+        reader = PdfReader(self.path)
+        text = ''
+        for page in reader.pages:
+            text += page.extract_text() + '\n'
         return text
+    
     def count_pages(self):
-        with open(self.path, 'rb') as file:
-            reader = PdfReader(file)
-            return len(reader.pages)
+        reader = PdfReader(self.path)
+        return len(reader.pages)
