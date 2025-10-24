5f1533c95784b842673b8ef74ccf5ccc67fe18b6
kaiPC <118703375+KaiEJH@users.noreply.github.com>
2025-10-23 21:43:08 -0400

    updated requirements to remove pyresparser (broken). Extraction now uses pyPdf.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
-from services.extraction import extract
+from services.extraction import PDF
 
 if __name__ == "__main__":
-    extract('./samples/sampleJosh.pdf')
+    pdf=PDF("samples/sampleJosh.pdf")
+    option = input("Enter '1' to extract text or '2' to count pages: ")
+    if option == '1':
+        print("Extracted Text:")
+        print(pdf.extract_text())
+    elif option == '2':
+        print(f"Number of pages: {pdf.count_pages()}")
--- app/services/extraction.py
+++ app/services/extraction.py
@@ -1,5 +1,16 @@
-from pyresparser import ResumeParser
+from pypdf import PdfReader
 
-def extract(path):
-    data = ResumeParser(path).get_extracted_data()
-    print(data)
+class PDF:
+    def __init__(self, path):
+        self.path = path
+    def extract_text(self):
+        with open(self.path, 'rb') as file:
+            reader = PdfReader(file)
+            text = ''
+            for page in reader.pages:
+                text += page.extract_text() + '\n'
+        return text
+    def count_pages(self):
+        with open(self.path, 'rb') as file:
+            reader = PdfReader(file)
+            return len(reader.pages)
