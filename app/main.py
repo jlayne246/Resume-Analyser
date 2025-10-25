4331040cc223ce16c8b0bbeda770062a2b4a34da
kaiPC <118703375+KaiEJH@users.noreply.github.com>
2025-10-24 22:18:26 -0400

    updated to add BERT for text classification

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from services.extraction import PDF
 
 if __name__ == "__main__":
-    pdf=PDF("samples/sampleJosh.pdf")
-    option = input("Enter '1' to extract text or '2' to count pages: ")
+    source = "samples/sampleMargaret.pdf"
+    pdf=PDF(source)
+    option = input("Pick an option\n1: Count pages\n2: Extract text\n3: Extract clean text\n4: Make summary\n5: Get Bert\n\nEnter option number: ")
     if option == '1':
+        print(f"Number of pages: {pdf.count_pages()}")
+    elif option == '2':
         print("Extracted Text:")
         print(pdf.extract_text())
-    elif option == '2':
-        print(f"Number of pages: {pdf.count_pages()}")
+    elif option == '3':
+        print("Extracted Text (clean):")
+        print(pdf.extract_clean_text())
+    elif option == '4':
+        print("Named Entities in the Document:")
+        pdf.make_summary()
+    elif option == '5':
+        print("BERT Model Output:")
+        outputs = pdf.test_bert()
+        print(outputs)
